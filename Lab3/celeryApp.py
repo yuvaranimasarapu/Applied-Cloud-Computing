ea554ba0bea0804e0398e211f9d99157b31ef469
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-10-22 04:47:57 +0200

    celery file changes 3

--- Lab3/celeryApp.py
+++ Lab3/celeryApp.py
@@ -9,9 +9,11 @@ def count_pronouns(tweets_file, word_list):
 		total_lines = my_file.readlines()
 		for line in total_lines:
 			try:
-				total_lines = total_lines.rstrip() #removing blank lines between consecutive tweets in our file
+				total_lines = total_lines.rstrip() 
+				#removing blank lines between consecutive tweets in our file
 				each_line = json.loads(line)
-				if not each_line['retweeted']: #check that tweet is not a retweet
+				if not each_line['retweeted']: 
+				#check that tweet is not a retweet
 					word_list['total'] +=1
 					tweet_content = each_line['text'].upper()
 					for pronoun in word_list.keys():
