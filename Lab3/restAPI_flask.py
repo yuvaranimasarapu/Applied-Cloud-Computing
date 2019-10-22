51525907f97ef3a7c846345fe72fd727d6c48d5d
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-10-22 04:37:44 +0200

    celery python file and flask app file added

--- Lab3/restAPI_flask.py
+++ Lab3/restAPI_flask.py
@@ -6,25 +6,23 @@ from functools import reduce
 
 app = Flask(__name__)
 
-pronounCount = {'HAN':0 , 'HON':0 , 'DEN':0 , 'DET':0 , 'DENNA':0 , 'DENNE':0 , 'HEN':0, 'total':0}
+word_list = {'HAN':0 , 'HON':0 , 'DEN':0 , 'DET':0 , 'DENNA':0 , 'DENNE':0 , 'HEN':0, 'total':0}
 
-@app.route (’/ getResult /’, methods =[ ’GET ’])
-def getPronouns () :
-queueResults = []
-for eachfile in os . listdir (’data ’) :
-queueResults . append ( countTweets (( ’/ home / ubuntu / data /’+ eachfile ) ,
-pronounCount ))
-time . sleep (10)
-return jsonify ( combineLists ( queueResults ))
+@app.route ('/pronounsCount/', methods =['GET'])
+def get_wordList():
+	temp = []
+	for eachfile in os.listdir('Lab3'):
+		temp.append(count_pronouns(('/home/ubuntu/Applied-Cloud-Computing/Lab3/data' + eachfile), word_list))
+	time.sleep(10)
+	return jsonify(appendWords(temp))
 
-def combineLists ( queueList ):
-def reducer ( accumulator , element ):
+def appendWords(temp):
+	def map_words_count(final_list_words, temp_list):
+		for pronoun,count_p in temp_list.items():
+			final_list_words[pronoun] = final_list_words.get(pronoun,0) + count_p
+		return final_list_words
 
-for key , value in element . items () :
-accumulator [ key ] = accumulator . get ( key , 0) + value
-return accumulator
+	return reduce(map_words_count,temp,{})
 
-return reduce ( reducer , queueList , {})
-
-if __name__ == ’__main__ ’:
-app . run ( host =’0.0.0.0 ’, debug = True )
+if __name__ == '__main__':
+	app.run(host ='0.0.0.0', debug = True )
