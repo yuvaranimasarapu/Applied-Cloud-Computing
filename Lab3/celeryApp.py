907774cfe8f24d4861de2e661f5411f40fd42bb2
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2019-10-24 21:08:44 +0200

    changes celery app

--- Lab3/celeryApp.py
+++ Lab3/celeryApp.py
@@ -8,17 +8,16 @@ app = Celery('celeryApp', backend='rpc://', broker='amqp://guest@localhost//')
 def count_pronouns(tweets_file, word_list):
 	with open(tweets_file,'r') as my_file:
 		total_lines = (line for line in my_file if line)
-		#total_lines = my_file.readlines()
-		for each_line in total_lines:
-			#try:
-			#each_line = json.loads(line)
+		for line in total_lines:
+			try:
+			each_line = json.loads(line)
 			if not each_line['retweeted']: 
 				#check that tweet is not a retweet
 				word_list['ALL_PRONOUNS'] +=1
 				tweet_content = each_line['text'].upper()
 				for eachPronoun in word_list.keys():
 					word_list[eachPronoun] += tweet_content.count(eachPronoun)
-			#except:
-				#continue
+			except:
+				continue
 		time.sleep(1) #to make asynchronous calls of celery workers to be synchronous
 		return word_list
